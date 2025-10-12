1deea14c36852dc30e9da5f2019b2e82262e4f2e
Cameron Dugan <cameron.dugan@protonmail.com>
2025-10-11 21:39:30 -0400

    add option for users to run via flake

--- flake.nix
+++ flake.nix
@@ -5,8 +5,17 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      pulsemixer = pkgs.writeShellScriptBin "pulsemixer" ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.pulseaudio]}:$LD_LIBRARY_PATH
+          exec ${pkgs.python3}/bin/python3 ${./pulsemixer} "$@"
+      '';
     in
     {
+      apps.default = {
+        type = "app";
+        program = "${pulsemixer}/bin/pulsemixer";
+        meta.description = "CLI and curses mixer for PulseAudio";
+      };
       devShell = pkgs.mkShell {
         packages = with pkgs; [
           python3
